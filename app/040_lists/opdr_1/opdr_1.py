2da17ca7b6fb7fbbe932bc14218bcef0c89c6ec4
97112022 <97112022@st.deltion.nl>
2025-09-18 23:26:11 +0200

    modified:   app/040_lists/opdr_1/opdr_1.py

--- app/040_lists/opdr_1/opdr_1.py
+++ app/040_lists/opdr_1/opdr_1.py
@@ -2,10 +2,11 @@
 # Naam student:
 # Groep:
 
-mylist = 
 dict_1 = {"id": 1, "voornaam": "Mark", "Achternaam": "van Laar"}  
 dict_2 = {"id": 2, "voornaam": "Jesper", "Achternaam": "Pot"}
 dict_3 = {"id": 3, "voornaam": "Niels", "Achternaam": "Verbree"}
 dict_4 = {"id": 4, "voornaam": "Eliot", "Achternaam": "Kuster"}
 
-print(mylist)
+mylist = [dict_1, dict_2, dict_3, dict_4] 
+
+print(mylist[1]["voornaam"], mylist[1]["Achternaam"])
